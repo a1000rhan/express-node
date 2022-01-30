647f343b75f110ba6fb43cf6feb5195660fdbd05
a1000rhan <1000rhan@gmail.com>
2022-01-30 11:00:20 +0300

    express
    q
    
    no

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ const express = require("express");
 const products = require("./products");
 const app = express();
 
-app.listen(8000, () => {
+app.listen(8001, () => {
   console.log("The application is running on localhost:8000");
 });
-app.get("/", (req, res) => {
-  console.log("HELLO");
-});
+
 app.get("/products", (req, res) => {
-  app.json(products);
+  res.json(products);
 });
